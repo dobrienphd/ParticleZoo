fb2c0bfaf6a709bbb8c4f551eaf035a8ed6f1ad6
Daniel O'Brien <daniel.obrien.phd@gmail.com>
2025-11-01 22:16:54 -0400

    Updated version to v1.1.0 beta

--- include/particlezoo/utilities/version.h
+++ include/particlezoo/utilities/version.h
@@ -58,7 +58,7 @@ namespace ParticleZoo {
          * built against the same major version should work with higher minor
          * versions without modification.
          */
-        static constexpr const int   MINOR_VERSION  = 0;
+        static constexpr const int   MINOR_VERSION  = 1;
 
         /**
          * @brief Patch version number.
@@ -67,7 +67,7 @@ namespace ParticleZoo {
          * that do not add new features. Applications should always be able to
          * upgrade to higher patch versions safely.
          */
-        static constexpr const int   PATCH_VERSION  = 1;
+        static constexpr const int   PATCH_VERSION  = 0;
 
         /**
          * @brief Development status indicator.
@@ -75,7 +75,7 @@ namespace ParticleZoo {
          * Indicates the current development status of this version. Only used to indicate 
          * pre-release or special build states.
          */
-        static constexpr const char* CAVEAT         = "";
+        static constexpr const char* CAVEAT         = "BETA";
 
         /**
          * @brief Generate a complete version string for display.
